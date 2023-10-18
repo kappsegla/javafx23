907f721261902db03845137d28ead815ac72d3a7
Martin Blomberg <martin.blomberg@outlook.com>
2023-10-18 14:06:19 +0200

    Clean project

--- src/main/java/com/example/tictactoe/HelloController.java
+++ src/main/java/com/example/tictactoe/HelloController.java
@@ -15,9 +15,6 @@ import java.util.stream.Stream;
 
 public class HelloController {
 
-    public Button saveButton;
-
-    public TextField textField;
 
     private Model model = new Model();
     //AudioClip buzzer = new AudioClip(getClass().getResource("sounds/punch.mp3").toExternalForm());
@@ -27,24 +24,8 @@ public class HelloController {
     }
 
     public void initialize() {
-        textField.textProperty().bindBidirectional(model.nameProperty());
+
     }
 
-    public void onAddButtonAction() {
-        model.addNewPerson();
-    }
 
-    public void onSaveButtonAction(ActionEvent event) {
-        Window window =
-                ((Node) event.getSource()).getScene().getWindow();
-        //ToDo: Reuse same fileChooser instance
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Save As");
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Text File", "*.txt"));
-        File selectedFile = fileChooser.showSaveDialog(window);
-        if (selectedFile != null) {
-            model.saveToFile(selectedFile.toPath());
-        }
-    }
 }
