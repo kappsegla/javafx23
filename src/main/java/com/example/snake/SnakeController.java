f96c6a74eaf4d524e8056c4b9d9dcbd6cb2b8e72
Martin Blomberg <martin.blomberg@outlook.com>
2023-10-20 09:39:42 +0200

    Add code for drawing on canvas

--- src/main/java/com/example/snake/SnakeController.java
+++ src/main/java/com/example/snake/SnakeController.java
@@ -1,11 +1,16 @@
 package com.example.snake;
 
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
 public class SnakeController {
 
+    public Canvas canvas;
 
     private Model model = new Model();
-    //AudioClip buzzer = new AudioClip(getClass().getResource("sounds/punch.mp3").toExternalForm());
+
 
     public Model getModel() {
         return model;
@@ -16,4 +21,13 @@ public class SnakeController {
     }
 
 
+    public void canvasClicked(MouseEvent mouseEvent) {
+        var xCoord = mouseEvent.getX();
+        var yCoord = mouseEvent.getY();
+
+        var gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        gc.setFill(Color.LAWNGREEN);
+        gc.fillRect(xCoord, yCoord, 15, 15);
+    }
 }
