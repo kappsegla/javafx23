5fc5144cccb48065aa5d6768ec8aea3c85d42647
Martin Blomberg <martin.blomberg@outlook.com>
2023-10-27 09:34:11 +0200

    Create crud-view.fxml

--- src/main/java/com/example/crud/HelloController.java
+++ src/main/java/com/example/crud/HelloController.java
@@ -1,9 +1,13 @@
 package com.example.crud;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class HelloController {
 
     private Model model = new Model();
+
 
     public Model getModel() {
         return model;
--- src/main/java/com/example/crud/Model.java
+++ src/main/java/com/example/crud/Model.java
@@ -1,5 +1,12 @@
 package com.example.crud;
 
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.control.TableView;
+
 public class Model {
 
+
+    public void test(){
+
+    }
 }
