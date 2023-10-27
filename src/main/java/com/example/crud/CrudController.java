eeeb8cecaecba8adeab63a36f8bcb6595b266f77
Martin Blomberg <martin.blomberg@outlook.com>
2023-10-27 11:29:39 +0200

    Disable delete button when no product selected.

--- src/main/java/com/example/crud/CrudController.java
+++ src/main/java/com/example/crud/CrudController.java
@@ -2,8 +2,10 @@ package com.example.crud;
 
 import com.example.crud.model.Model;
 import com.example.crud.model.Product;
+import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -19,6 +21,7 @@ public class CrudController {
     public TextField productName;
     public TextField productPrice;
     public TextField productCategory;
+    public Button deleteButton;
 
     Model model = new Model();
 
@@ -27,6 +30,8 @@ public class CrudController {
         nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
         priceColumn.setCellValueFactory(cellData -> cellData.getValue().priceProperty().asObject());
         model.selectedProductProperty().bind(tableView.getSelectionModel().selectedItemProperty());
+        deleteButton.disableProperty().bind( Bindings.isNull(model.selectedProductProperty()) );
+
     }
 
 
