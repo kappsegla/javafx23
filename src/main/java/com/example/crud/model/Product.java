a671a2620b39be8a5d36c53a162733177ce55c58
Martin Blomberg <martin.blomberg@outlook.com>
2023-10-27 10:10:30 +0200

    Create Product model

--- src/main/java/com/example/crud/model/Product.java
+++ src/main/java/com/example/crud/model/Product.java
@@ -1,6 +1,70 @@
 package com.example.crud.model;
 
+import javafx.beans.property.*;
+
+import java.util.Objects;
+
 public class Product {
 
+    StringProperty name = new SimpleStringProperty("");
 
+    StringProperty category = new SimpleStringProperty("");
+
+    IntegerProperty price = new SimpleIntegerProperty(0);
+
+
+    public Product(String name, String category, int price) {
+        this.name.set(name);
+        this.category.set(category);
+        this.price.set(price);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getCategory() {
+        return category.get();
+    }
+
+    public StringProperty categoryProperty() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category.set(category);
+    }
+
+    public int getPrice() {
+        return price.get();
+    }
+
+    public IntegerProperty priceProperty() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price.set(price);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && Objects.equals(category, product.category) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, category, price);
+    }
 }
